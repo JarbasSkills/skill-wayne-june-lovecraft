68b2070737b1d51845da11bff676227f6a7b062e
jarbasal <jarbasai@mailfence.com>
2021-08-14 18:20:15 +0100

    common playing

--- __init__.py
+++ __init__.py
@@ -98,7 +98,7 @@ class WayneJuneLovecraftReadingsSkill(OVOSCommonPlaybackSkill):
                 "match_confidence": CommonPlayMatchConfidence.HIGH,
                 "media_type":  CPSMatchType.MUSIC,
                 "uri": "https://audioservice.or.gui.will.play.this",
-                "playback": CommonPlayPlaybackType.GUI,
+                "playback": CommonPlayPlaybackType.VIDEO,
                 "image": "http://optional.audioservice.jpg",
                 "bg_image": "http://optional.audioservice.background.jpg"
             }
